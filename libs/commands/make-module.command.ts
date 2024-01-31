8917804580fbf0b45d6f8b1613e0c4a5fc807e55
phuong-hodfords <phuong.vt@hodfords.com>
2024-01-31 21:57:10 +0700

    feat: upgrade nestjs 10

--- libs/commands/make-module.command.ts
+++ libs/commands/make-module.command.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { BaseMakeCommand } from './base-make.command';
+import { resolve } from 'path';
+import pluralize from 'pluralize';
 import { Command } from '../decorators/command.decorator';
-import { MakeE2eTestCommand } from './make-e2e-test.command';
-import { MakeServiceCommand } from './make-service.command';
+import { BaseMakeCommand } from './base-make.command';
 import { MakeControllerCommand } from './make-controller.command';
+import { MakeDtoCommand } from './make-dto.command';
+import { MakeE2eTestCommand } from './make-e2e-test.command';
 import { MakeEntityCommand } from './make-entity.command';
 import { MakeRepositoryCommand } from './make-repository.command';
-import { MakeDtoCommand } from './make-dto.command';
-import { resolve } from 'path';
+import { MakeServiceCommand } from './make-service.command';
 
 @Command({
     signature: 'make-module <name>',
-    description: 'Make a migration'
+    description: 'Make a new module'
 })
 @Injectable()
 export class MakeModuleCommand extends BaseMakeCommand {
@@ -27,17 +28,45 @@ export class MakeModuleCommand extends BaseMakeCommand {
     }
 
     public getStub() {
-        return resolve(__dirname, '../stubs/make-migration.stub');
+        return resolve(__dirname, '../stubs/modules/module.stub');
+    }
+
+    get moduleName() {
+        return pluralize(this.args[0]);
     }
 
     public handle() {
         let [name] = this.args;
-        let options = { module: name };
+        let options = { module: this.moduleName };
         this.makeTestCommand.runWith([`${name}.controller`], options);
         this.makeServiceCommand.runWith([name], options);
         this.makeControllerCommand.runWith([name], options);
         this.makeEntityCommand.runWith([name], options);
         this.makeRepositoryCommand.runWith([name], options);
         this.makeDtoCommand.runWith([`create-${name}`], options);
+
+        this.createModuleFile();
+    }
+
+    private createModuleFile() {
+        let [name] = this.args;
+        this.getContent();
+        this.replaceContent([
+            {
+                search: '$$CLASS$$',
+                value: this.getClassName(name)
+            },
+            {
+                search: '$$CONTROLLER_NAME$$',
+                value: this.moduleName
+            },
+            {
+                search: '$$FILENAME$$',
+                value: this.getFileName(name)
+            }
+        ]);
+
+        this.writeFileToModule(this.moduleName, `${name}.module.ts`);
+        this.success(`Create ${this.moduleName} successfully!`);
     }
 }
--- libs/index.ts
+++ libs/index.ts
@@ -1,5 +1,5 @@
 export * from './command.module';
 export * from './command.service';
-export * from './decorators/command.decorator';
-export * from './commands/base.command';
 export * from './commands/base-make.command';
+export * from './commands/base.command';
+export * from './decorators/command.decorator';
